39007fbc078b8f47cc430981e5b834b51e58fbde
sabreshao <sabre.shao@amd.com>
2019-07-04 17:58:56 +0800

    Fix oclloader load asmv2.

--- examples/asmv2/cov2.s
+++ examples/asmv2/cov2.s
@@ -26,3 +26,51 @@ hello_world:
   s_endpgm
 .Lfunc_end0:
      .size   hello_world, .Lfunc_end0-hello_world
+
+
+.amd_amdgpu_hsa_metadata
+  Version: [ 1, 0 ]
+  Printf: [ '1:1:4:%d\n', '2:1:8:%g\n' ]
+  Kernels:
+    - Name:            hello_world
+      SymbolName:      hello_world@kd
+      Language:        OpenCL C
+      LanguageVersion: [ 2, 0 ]
+      Attrs:
+        ReqdWorkGroupSize: [ 1, 2, 4 ]
+        WorkGroupSizeHint: [ 8, 16, 32 ]
+        VecTypeHint:       int
+      CodeProps:
+        KernargSegmentSize:      24
+        GroupSegmentFixedSize:   24
+        PrivateSegmentFixedSize: 16
+        KernargSegmentAlign:     16
+        WavefrontSize:           64
+        MaxFlatWorkGroupSize:    256
+        NumSpilledSGPRs:         1
+        NumSpilledVGPRs:         1
+      Args:
+        - TypeName:      char
+          Size:          1
+          Align:         1
+          ValueKind:     ByValue
+          ValueType:     I8
+          AccQual:       Default
+        - Size:          8
+          Align:         8
+          ValueKind:     HiddenGlobalOffsetX
+          ValueType:     I64
+        - Size:          8
+          Align:         8
+          ValueKind:     HiddenGlobalOffsetY
+          ValueType:     I64
+        - Size:          8
+          Align:         8
+          ValueKind:     HiddenGlobalOffsetZ
+          ValueType:     I64
+        - Size:          8
+          Align:         8
+          ValueKind:     HiddenPrintfBuffer
+          ValueType:     I8
+          AddrSpaceQual: Global
+.end_amd_amdgpu_hsa_metadata
